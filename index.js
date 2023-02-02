7fe49e8aa77e3f2c35487785a28798fe9add7183
Aman Kumar <kumaramansingh137@gmail.com>
2023-02-02 18:22:16 +0530

    sec commit

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 
 
-var x = 7;
+//var x = 7;
 
-function getName() {
-    console.log("Namaste Javascript");
-}
-getName();
-console.log(x);
-console.log(getName);
+//function getName() {
+  //  console.log("Namaste Javascript");
+//}
+//getName();
 //console.log(x);
+//console.log(getName);
+//console.log(x);
+
+
+var x = 1;
+a();
+b();
+console.log(x);
+
+
+function a() {
+    var x = 10;
+    console.log(x);
+}
+
+function b() {
+    var x = 100;
+    console.log(x);
+}

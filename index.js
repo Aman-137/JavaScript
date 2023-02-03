dceb9d5b718d9287e1479d9ccf263c42fadcce06
Aman Kumar <kumaramansingh137@gmail.com>
2023-02-03 17:44:41 +0530

    scope, lexical env, scope chain

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 
-
-//var x = 7;
-
-//function getName() {
-  //  console.log("Namaste Javascript");
-//}
-//getName();
-//console.log(x);
-//console.log(getName);
-//console.log(x);
-
-
-var x = 1;
-a();
-b();
-console.log(x);
-
-
 function a() {
-    var x = 10;
-    console.log(x);
+    var b = 10;
+    c();
+    function c() {
+        console.log(b);
+    }
 }
 
-function b() {
-    var x = 100;
-    console.log(x);
-}
+
+a();

68ec212aa900e40ffb207ab9f3556cf736a2498e
Aman Kumar <kumaramansingh137@gmail.com>
2023-02-06 17:38:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
+/*let b = 100;
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
 
-function a() {
-    var b = 10;
-    c();
-    function c() {
-        console.log(b);
-    }
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
+console.log(b);*/
 
-a();
+/*const c = 100;
+
+function x() {
+    const c = 30;
+    console.log(c);
+}
+x();
+console.log(c);*/
+
+/*const a = 20;
+{
+    const a = 100;
+    {
+        const a = 200;
+        
+    }
+
+}
+console.log(a)*/
+
+
+/*function x() {
+    var a = 7;
+    function y() {
+        console.log(a);
+    }
+    a = 100;
+    return y;
+}
+var z = x();
+
+console.log(z);
+
+z();*/
+
+
+function z() {
+    var b = 900;
+    function x() {
+        var a = 7;
+        function y() {
+            console.log(a, b);
+        }
+        y();
+    }
+    x();
+}
+z();

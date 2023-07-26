a56cde37ce48d17384571f3678a0f5bba9f3abf6
Aman Kumar <kumaramansingh137@gmail.com>
2023-07-27 00:39:51 +0530

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1460,7 +1460,7 @@ function importantAction(username) {
 function likeTheVideo(video) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      reject(`like the ${video} video`);
+      resolve(`like the ${video} video`);
     }, 1000);
   });
 }
@@ -1549,8 +1549,28 @@ Promise.any([
   .catch((err) => {
     console.error("Error: Promises failed", err);
   });
-*/
 
 // Another Approach async / await
+const result = async () => {
+  try {
+    const message1 = await importantAction("Roadside Coder");
+    console.log(message1);
+    const message2 = await likeTheVideo("Javascript Interview Questions");
+    console.log(message2);
+    const message3 = await shareTheVideo("Javascript Interview Questions");
+    console.log(message3);
+
+    // console.log({ message1, message2, message3 });
+  } catch (error) {
+    console.error("Promises Failed", error);
+  }
+};
+
+result();
 
 console.log("stop");
+*/
+
+// Ouput based question on Promises
+
+// Q1 - what's the output?

43fa0d171918a211b048d99c9c730b64cd2fdb71
Aman Kumar <kumaramansingh137@gmail.com>
2023-08-01 19:49:25 +0530

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1708,3 +1708,70 @@ promiseRecursive([
 */
 
 // Q10 - Promise polyfill implementation (VVVI)
+
+function PromisePolyFill(executor) {
+  let onResolve,
+    onReject,
+    isFullfilled = false,
+    isRejected = false,
+    isCalled = false,
+    value;
+
+  function resolve(val) {
+    isFullfilled = true;
+    value = val;
+
+    if (typeof onResolve === "function") {
+      onResolve(val);
+      isCalled = true;
+    }
+  }
+
+  function reject(val) {
+    isRejected = true;
+    value = val;
+
+    if (typeof onReject === "function") {
+      onReject(val);
+      called = true;
+    }
+  }
+
+  this.then = function (callback) {
+    onResolve = callback;
+
+    if (isFullfilled && !isCalled) {
+      called = true;
+      onResolve(value);
+    }
+    return this;
+  };
+
+  this.catch = function (callback) {
+    onReject = callback;
+
+    if (isRejected && !isCalled) {
+      isCalled = true;
+      onReject(value);
+    }
+    return this;
+  };
+
+  try {
+    executor(resolve, reject);
+  } catch (error) {
+    reject(error);
+  }
+}
+
+const examplePromise = new PromisePolyFill((resolve, reject) => {
+  setTimeout(() => {
+    reject(2);
+  }, 1000);
+});
+
+examplePromise
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => console.error(err));

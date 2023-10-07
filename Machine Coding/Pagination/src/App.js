a0f15b4268c53263077e37201db863a9be8686d7
Aman Kumar <kumaramansingh137@gmail.com>
2023-10-07 23:19:18 +0530

    react/medium

---
+++ Machine Coding/Pagination/src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await res.json();
+
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  };
+
+  console.log(products);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      {products.length > 0 && (
+        <div className="products">
+          {products.map((prod) => {
+            return (
+              <span className="products__single" key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}

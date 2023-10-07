b135bb08a47fa3f09af59582b2798caab3679d01
Aman Kumar <kumaramansingh137@gmail.com>
2023-10-08 03:25:17 +0530

    Update App.js

--- Machine Coding/Pagination/src/App.js
+++ Machine Coding/Pagination/src/App.js
@@ -3,6 +3,7 @@ import "./styles.css";
 
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchProducts = async () => {
     const res = await fetch("https://dummyjson.com/products?limit=100");
@@ -23,7 +24,7 @@ export default function App() {
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.map((prod) => {
+          {products.slice(page * 10 - 10, page * 10).map((prod) => {
             return (
               <span className="products__single" key={prod.id}>
                 <img src={prod.thumbnail} alt={prod.title} />
@@ -31,6 +32,16 @@ export default function App() {
               </span>
             );
           })}
+        </div>
+      )}
+
+      {products.length > 0 && (
+        <div className="pagination">
+          <span>◀️</span>
+          {[...Array(products.length / 10)].map((_, i) => {
+            return <span key={i}>{i + 1}</span>;
+          })}
+          <span>▶️</span>
         </div>
       )}
     </div>
